8b47792cf0b9c2833d99bc81fee6f5c79177fa6b
paulngch <30496630+paulngch@users.noreply.github.com>
2023-01-18 02:09:23 +0800

    Added crypto to gen randnames for s3 images

--- server/controllers/cablesController.js
+++ server/controllers/cablesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 require("dotenv").config();
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
+const crypto = require("crypto");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+const randomImageName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
 
 const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
@@ -21,21 +24,25 @@ const s3 = new S3Client({
   region: bucketRegion,
 });
 
+//===============================
+//Uploading (POST) image to S3 & sending to mongo
 router.post("/", upload.single("image"), async (req, res) => {
   console.log("req.body", req.body);
   console.log("req.file", req.file);
 
   req.file.buffer;
-
+  const imageName = randomImageName();
   const params = {
     Bucket: bucketName,
-    Key: req.file.originalname,
+    Key: imageName,
     Body: req.file.buffer,
     ContentType: req.file.mimetype,
   };
 
   const command = new PutObjectCommand(params);
   await s3.send(command);
+
+
 
   res.send({});
 });
