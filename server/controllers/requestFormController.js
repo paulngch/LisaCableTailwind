c908e52cd4d0841928174bf7c9c501d7685c6af2
paulngch <30496630+paulngch@users.noreply.github.com>
2023-02-07 12:15:37 +0800

    Reinstalled dotenv

--- server/controllers/requestFormController.js
+++ server/controllers/requestFormController.js
@@ -15,20 +15,19 @@ router.post("/", async (req, res) => {
     //creating form from schema, sending to mongoDB
     const requestForm = await RequestForm.create(req.body);
 
-    const msg = {
-      to: "TinyLisa@pm.me", // recipient
-      from: "paulngch@gmail.com", // verified sender
-      subject: `SendGrid: Cable Commission Request from ${data.name} , ${data.email}`,
-      text: `${data.email} , ${data.contact} , ${data.country} , ${data.comments}`,
-      html: `Email: ${data.email},
-      Contact: ${data.contact},
-      Name: ${data.name},
-      Discord: ${data.discord},
-      Country: ${data.country},
-      HostUSB: ${data.hostUsb},
-      DeviceUSB: ${data.deviceUsb},
-      Comments: ${data.comments}`,
-    };
+    // const msg = {
+    //   to: "TinyLisa@pm.me", // recipient
+    //   from: "paulngch@gmail.com", // verified sender
+    //   subject: `SendGrid: Cable Commission Request from ${data.name} , ${data.email}`,
+    //   text: `${data.email} , ${data.contact} , ${data.country} , ${data.comments}`,
+    //   html: `Email: ${data.email} ${(<br />)} Contact: ${data.contact} ${(
+    //     <br />
+    //   )} Name: ${data.name} ${(<br />)} Discord: ${data.discord} ${(
+    //     <br />
+    //   )} Country: ${data.country} ${(<br />)} HostUSB: ${data.hostUsb} ${(
+    //     <br />
+    //   )} DeviceUSB: ${data.deviceUsb} ${(<br />)} Comments: ${data.comments}`,
+    // };
 
     res.status(201).json(requestForm);
   } catch (error) {
